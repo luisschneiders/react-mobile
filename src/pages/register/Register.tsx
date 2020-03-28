551a006bdcee54154aa1f67fbb99ff1dbfba11ec
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-03-28 16:22:46 +1100

    [Register] - implement registerUser with firebase

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -10,14 +10,29 @@ import {
 } from '@ionic/react';
 import './Register.css';
 import { Link } from 'react-router-dom';
+import { toast } from '../../components/toast/Toast';
+import { registerUser } from '../../config/Firebase';
 
 const Register: React.FC = () => {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   
-  function registerUser() {
-    
+  async function register() {
+    if (password !== confirmPassword) {
+      return toast(`Passwords should match!`, 'warning');
+    }
+
+    if (email.trim() === '' || password.trim() === '') {
+      return toast(`Email and password are required!`, 'warning');
+    }
+
+    const response: any = await registerUser(email, password);
+    if (response) {
+      // Go to dashboard...
+    } else {
+      
+    }
   }
 
   return (
@@ -34,15 +49,15 @@ const Register: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <IonContent>
-          <IonInput type="email" placeholder="Username"
-                    onIonChange={(e: any) => setUsername(e.target.value)}></IonInput>
+          <IonInput type="email" placeholder="Email"
+                    onIonChange={(e: any) => setEmail(e.target.value)}></IonInput>
 
           <IonInput type="password" placeholder="Password"
                     onIonChange={(e: any) => setPassword(e.target.value)}></IonInput>
 
           <IonInput type="password" placeholder="Confirm Password"
                     onIonChange={(e: any) => setConfirmPassword(e.target.value)}></IonInput>
-          <IonButton color="medium" expand="full" onClick={registerUser} >Register</IonButton>
+          <IonButton color="medium" expand="full" onClick={register} >Register</IonButton>
           <span>Already have an account? <Link to="/login">Login</Link></span>
         </IonContent>
       </IonContent>
