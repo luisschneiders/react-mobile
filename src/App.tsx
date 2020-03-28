7265b0bb905623ce726f8eb493267db458614cd0
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-03-28 12:06:25 +1100

    add routing and components for login and register steps

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,16 @@ import {
   IonTabs
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import {
+  cogOutline,
+  addOutline,
+  briefcaseOutline 
+} from 'ionicons/icons';
 import Tab1 from './pages/tab1/Tab1';
 import Tab2 from './pages/tab2/Tab2';
 import Tab3 from './pages/tab3/Tab3';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -35,32 +41,34 @@ import '@ionic/react/css/display.css';
 import './theme/variables.css';
 
 const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route path="/tab1" component={Tab1} exact={true} />
-          <Route path="/tab2" component={Tab2} exact={true} />
-          <Route path="/tab3" component={Tab3} />
-          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
+    <IonApp>
+        <IonReactRouter>
+          <IonTabs>
+            <IonRouterOutlet>
+              <Route path='/login' component={Login} exact={true} />
+              <Route path='/register' component={Register} exact={true} />
+              <Route path='/page1' component={Tab1} exact={true} />
+              <Route path='/page2' component={Tab2} exact={true} />
+              <Route path='/settings' component={Tab3} />
+              <Route path='/' render={() => <Redirect to='/login' />} exact={true} />
+            </IonRouterOutlet>
+            <IonTabBar slot='bottom'>
+              <IonTabButton tab='tab1' href='/page1'>
+                <IonIcon icon={briefcaseOutline} />
+                <IonLabel>Page 1</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab='tab2' href='/page2'>
+                <IonIcon icon={addOutline} />
+                <IonLabel>Page 2</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab='tab3' href='/settings'>
+                <IonIcon icon={cogOutline} />
+                <IonLabel>Settings</IonLabel>
+              </IonTabButton>
+            </IonTabBar>
+          </IonTabs>
+        </IonReactRouter>
+    </IonApp>
 );
 
 export default App;
