83771f70033e589e545a6a0b4a501e35b0da93ef
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-03-28 13:02:53 +1100

    [Login] - update UI, set react hook useState

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   IonContent,
   IonHeader,
   IonPage,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonInput,
+  IonButton
 } from '@ionic/react';
 import './Login.css';
 
 const Login: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function loginUser() {
+    
+  }
+
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader class="ion-text-center">
         <IonToolbar>
           <IonTitle>Login</IonTitle>
         </IonToolbar>
@@ -22,6 +31,13 @@ const Login: React.FC = () => {
             <IonTitle size="large">Login</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonContent>
+          <IonInput placeholder='Username'
+                    onIonChange={(e: any) => setUsername(e.target.value)}></IonInput>
+          <IonInput placeholder='Password'
+                    onIonChange={(e: any) => setPassword(e.target.value)}></IonInput>
+          <IonButton color='light' expand='full'onClick={loginUser} >Login</IonButton>
+        </IonContent>
       </IonContent>
     </IonPage>
   );
