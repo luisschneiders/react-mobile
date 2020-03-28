ab6c878fc9f859616c158b967216fde4ef4a3222
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-03-28 14:41:49 +1100

    [Register] - update UI

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   IonContent,
   IonHeader,
   IonPage,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonInput,
+  IonButton
 } from '@ionic/react';
 import './Register.css';
+import { Link } from 'react-router-dom';
 
 const Register: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  
+  function registerUser() {
+    
+  }
+
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader class="ion-text-center">
         <IonToolbar>
           <IonTitle>Register</IonTitle>
         </IonToolbar>
@@ -22,6 +33,18 @@ const Register: React.FC = () => {
             <IonTitle size="large">Register</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonContent>
+          <IonInput type="email" placeholder="Username"
+                    onIonChange={(e: any) => setUsername(e.target.value)}></IonInput>
+
+          <IonInput type="password" placeholder="Password"
+                    onIonChange={(e: any) => setPassword(e.target.value)}></IonInput>
+
+          <IonInput type="password" placeholder="Confirm Password"
+                    onIonChange={(e: any) => setConfirmPassword(e.target.value)}></IonInput>
+          <IonButton color="medium" expand="full" onClick={registerUser} >Register</IonButton>
+          <span>Already have an account? <Link to="/login">Login</Link></span>
+        </IonContent>
       </IonContent>
     </IonPage>
   );
