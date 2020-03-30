8d1149935de7ce242dd6f056b13023ed64087695
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-03-31 10:39:19 +1100

    [Index] - update app location

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
