541f23608e12033093ce978da39343111e35d018
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-04-03 08:05:36 +1100

    [App] - add levels for pages

--- src/app/AppPage.ts
+++ src/app/AppPage.ts
@@ -2,4 +2,5 @@ export interface AppPage {
   url: string;
   label: string;
   icon: string;
+  level?: string;
 }
--- src/app/AppPages.ts
+++ src/app/AppPages.ts
@@ -3,22 +3,24 @@ import {
   addOutline,
   cogOutline,
   albumsOutline,
-  logOut
+  logOut,
+  peopleOutline
 } from "ionicons/icons";
 import { AppPage } from "./AppPage";
 
 export function appPages() {
   const authenticated: AppPage[] = [
-    { url: '/tabs/tab1', label: 'Tab 1 Auth', icon: homeOutline },
-    { url: '/tabs/tab2', label: 'Tab 2 Auth', icon: addOutline },
-    { url: '/tabs/tab3', label: 'Tab 3 Auth', icon: cogOutline },
-    { url: '/logout', label: 'Logout', icon: logOut }
+    { level: 'Menu', url: '/tabs/tab1', label: 'Tab 1 Auth', icon: homeOutline },
+    { level: 'Menu', url: '/tabs/tab2', label: 'Tab 2 Auth', icon: addOutline },
+    { level: 'Menu', url: '/tabs/tab3', label: 'Tab 3 Auth', icon: cogOutline },
+    { level: 'Settings', url: '/account', label: 'Account', icon: peopleOutline },
+    { level: '', url: '/logout', label: 'Logout', icon: logOut }
   ];
 
   const unauthenticated: AppPage[] = [
-    { url: '/tabs/tab1', label: 'Tab 1 Unauth', icon: homeOutline },
-    { url: '/tabs/tab2', label: 'Tab 2 Unauth', icon: albumsOutline },
-    { url: '/tabs/tab3', label: 'Tab 3 Unauth', icon: cogOutline },
+    { level: 'Menu', url: '/tabs/tab1', label: 'Tab 1 Unauth', icon: homeOutline },
+    { level: 'Menu', url: '/tabs/tab2', label: 'Tab 2 Unauth', icon: albumsOutline },
+    { level: 'Menu', url: '/tabs/tab3', label: 'Tab 3 Unauth', icon: cogOutline },
   ];
 
   const pages: any = Object.assign({}, {
