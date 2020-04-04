dfd5429cf00a5fb7c8830a8deb26c458a57a28b0
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-04-04 20:15:39 +1100

    [Account] - component UI and state updates

--- src/pages/account/Account.tsx
+++ src/pages/account/Account.tsx
@@ -14,19 +14,42 @@ import {
   IonItem,
   IonLabel,
   IonInput,
-  IonLoading
+  IonLoading,
+  IonAvatar
 } from '@ionic/react';
 import './Account.scss';
 import { updateUser } from '../../config/Firebase';
+import { toast } from '../../components/toast/Toast';
+import { ToastStatus } from '../../components/toast/ToastStatus';
+import { delay } from '../../util/delay';
+import { RouteComponentProps } from 'react-router';
+import { setDisplayName } from '../../data/user/user.actions';
+import { connect } from '../../data/connect';
 
-const Account: React.FC = () => {
-  const [displayName, setDisplayName] = useState<any>();
+
+interface OwnProps extends RouteComponentProps {}
+interface StateProps {
+  displayName?: string
+}
+interface DispatchProps {
+  setDisplayName: typeof setDisplayName
+}
+
+interface AccountProps extends OwnProps, StateProps, DispatchProps {}
+
+const Account: React.FC<AccountProps> = ({ setDisplayName, displayName }) => {
+  const [username, setUsername] = useState();
   const [busy , setBusy] = useState(false);
   const account = async (e: React.FormEvent) => {
     e.preventDefault();
     setBusy(true);
-    const response: any = await updateUser(displayName);
+    const response: any = await updateUser(username);
+    await delay(500);
     setBusy(false);
+    if (response) {
+      setDisplayName(username);
+      toast('Successfully updated!', ToastStatus.DEFAULT);
+    }
   }
 
   return (
@@ -41,13 +64,19 @@ const Account: React.FC = () => {
       </IonHeader>
       <IonLoading message="Updating..." duration={0} isOpen={busy}></IonLoading>
       <IonContent className="ion-padding">
-      <form noValidate onSubmit={account}>
+        <form noValidate onSubmit={account}>
+          <div className="account account__avatar">
+            <IonAvatar>
+              <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
+            </IonAvatar>
+            <h2>{username ? username : displayName}</h2>
+          </div>
           <IonList>
             <IonItem>
               <IonLabel position="stacked" color="primary">Username</IonLabel>
-              <IonInput name="displayName" type="text"
-                        value={displayName} spellCheck={false} autocapitalize="off"
-                        onIonChange={(e: any) => setDisplayName(e.detail.value!)}
+              <IonInput name="username" type="text"
+                        value={username} spellCheck={false} autocapitalize="off"
+                        onIonChange={(e: any) => setUsername(e.detail.value!)}
                         required>
               </IonInput>
             </IonItem>
@@ -63,4 +92,14 @@ const Account: React.FC = () => {
   );
 };
 
-export default Account;
+// export default Account;
+
+export default connect<OwnProps, StateProps, DispatchProps>({
+  mapStateToProps: (state) => ({
+    displayName: state.user.displayName
+  }),
+  mapDispatchToProps: {
+    setDisplayName
+  },
+  component: Account
+})
