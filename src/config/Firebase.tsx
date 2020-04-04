32052babac2a987db837887dd4268387c06d637a
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-04-04 20:06:47 +1100

    [Firebase] - update user profile

--- src/config/Firebase.tsx
+++ src/config/Firebase.tsx
@@ -9,7 +9,7 @@ firebase.initializeApp(Firebase);
 export async function loginUser(email: string, password: string) {
   try {
     const response: any = await firebase.auth().signInWithEmailAndPassword(email, password);
-    toast('Welcome back!', ToastStatus.DEFAULT);
+    toast(`Welcome back ${response?.user?.displayName}!`, ToastStatus.DEFAULT);
     return response;
   } catch(error) {
     toast(error.message, ToastStatus.ERROR, 4000);
@@ -30,7 +30,7 @@ export async function registerUser(email: string, password: string) {
 
 export function getCurrentUser() {
   return new Promise((resolve, reject) => {
-    const unsubscribe: any = firebase.auth().onAuthStateChanged((user) => {
+    const unsubscribe: any = firebase.auth().onAuthStateChanged((user: any) => {
       if (user) {
         resolve(user);
       } else {
@@ -46,11 +46,11 @@ export async function logoutUser() {
   return response;
 }
 
-export async function updateUser(user: firebase.User) {
+export async function updateUser(username: string | undefined) {
   try {
-    const response: any = await firebase.auth().updateCurrentUser(user);
-    toast('Successfully updated!', ToastStatus.DEFAULT);
-    return response;
+    firebase.auth().currentUser?.updateProfile({displayName: username});
+    return true;
+
   } catch(error) {
     toast(error.message, ToastStatus.ERROR, 4000);
     return false;
