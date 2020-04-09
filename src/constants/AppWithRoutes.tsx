d07caca506aeb309a73c321e002ab5189c6b8e41
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-04-09 20:10:27 +1000

    [Constants] - create routes constants

---
+++ src/constants/AppWithRoutes.tsx
@@ -0,0 +1,13 @@
+/**
+ * Constants are listed in alphabetical order
+ */
+
+export const ACCOUNT = '/account';
+export const HOME = '/home';
+export const LOGIN = '/login';
+export const LOGOUT = '/logout';
+export const REGISTER = '/register';
+export const TABS = '/tabs';
+export const TAB1 = `${TABS}/tab1`;
+export const TAB2 = `${TABS}/tab2`;
+export const TAB3 = `${TABS}/tab3`;
