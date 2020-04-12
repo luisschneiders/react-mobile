4223fa99b093ec9156b567992c1bd021b7805f74
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-04-13 09:20:03 +1000

    [Pages] - update UI for tab

--- src/pages/tab1/Tab1.tsx
+++ src/pages/tab1/Tab1.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   IonContent,
   IonHeader,
@@ -6,12 +6,43 @@ import {
   IonTitle,
   IonToolbar,
   IonButtons,
-  IonMenuButton
+  IonMenuButton,
+  IonList,
+  IonLoading,
+  IonItem,
+  IonLabel,
+  IonImg,
+  IonThumbnail,
 } from '@ionic/react';
-import ExploreContainer from '../../components/ExploreContainer';
 import './Tab1.scss';
+import { generalNews } from '../../data/api/Finnhub';
+import { NewsType } from '../../enum/NewsType';
+import GroupList from '../../components/list/GroupList';
 
 const Tab1: React.FC = () => {
+  const [isError, setError] = useState<boolean>(false);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [items, setItems] = useState<any>();
+  const noNews: string = 'assets/img/no-news.jpg';
+
+  // TODO: add state, actions and reducer
+
+  useEffect(() => {
+    setIsLoaded(true);
+
+    generalNews(NewsType.GENERAL, 5).then((response: any) => {
+      if (Object.keys(response).length > 0) {
+          setItems(response);
+          setIsLoaded(false);
+      }
+      else {
+        setIsLoaded(false);
+        setError(true);
+      }
+    });
+
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
@@ -22,13 +53,19 @@ const Tab1: React.FC = () => {
           <IonTitle>Tab 1</IonTitle>
         </IonToolbar>
       </IonHeader>
+      <IonLoading message="Fetching news..." duration={0} isOpen={isLoaded}></IonLoading>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        {isError && <IonList>
+          <IonItem>
+            <IonThumbnail slot="start">
+              <IonImg src={noNews} alt='no news' />
+            </IonThumbnail>
+            <IonLabel>
+              <h2>No news found!</h2>
+            </IonLabel>
+          </IonItem>
+        </IonList>}
+        {items && <GroupList data={items}></GroupList>}
       </IonContent>
     </IonPage>
   );
