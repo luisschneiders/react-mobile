5bf067327f02cf3ce1ba21f82e69756d8b794b59
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-04-15 12:58:31 +1000

    [Data] - add local api call for testing

--- src/data/api/Finnhub.tsx
+++ src/data/api/Finnhub.tsx
@@ -7,7 +7,7 @@ import { News, Category } from '../../models/News';
 import { groupBy } from '../../util/groupBy';
 
 // To use this one...
-export function generalNews(category: string = NewsType.GENERAL, minId: number = 10) {
+export function getNews(category: string = NewsType.GENERAL, minId: number = 10) {
   return fetch(`https://finnhub.io/api/v1/news?category=${category}&${minId}&token=${Finnhub.token}`)
     .then(response => response.json())
     .then(
@@ -17,10 +17,10 @@ export function generalNews(category: string = NewsType.GENERAL, minId: number =
           const list: List = Object.assign({}, {
             category: item.category,
             id: item.id,
-            src: item.image,
+            image: item.image,
             alt: item.headline,
-            h2: item.headline,
-            p: item.summary,
+            headline: item.headline,
+            summary: item.summary,
           })
           customList.push(list);
         });
@@ -43,4 +43,43 @@ export function generalNews(category: string = NewsType.GENERAL, minId: number =
         return error;
       }
     );
+}
+
+export function getNewsLocal(category: string = NewsType.GENERAL, minId: number = 10) {
+  return new Promise((resolve, reject) => {
+    const data: any[] = require('./local/news.json');
+
+    // Get only data required for the list
+    const customList: List[] = [];
+    data.forEach((item: any) => {
+      const list: List = Object.assign({}, {
+        category: item.category,
+        id: item.id,
+        image: item.image,
+        alt: item.headline,
+        headline: item.headline,
+        summary: item.summary,
+      })
+      customList.push(list);
+    });
+
+    // Because the original data is not grouped by 'category',
+    // we need to do it here.
+    const groups: Category[] = [];
+    const customData: any = groupBy(customList, 'category');
+    Object.keys(customData).forEach((key: string) => {
+      groups.push(customData[key]);
+    });
+
+    // Assign the new array group to the News
+    const news: News = Object.assign({}, {
+      groups
+    });
+
+    if (customList) {
+      resolve(news);
+    } else {
+      resolve(null);
+    }
+  });
 }
